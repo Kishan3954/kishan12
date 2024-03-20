d8c9f1a1dc55ed94466123a0689b18eb4b81e0cb
Kishan <154419159+Kishan3954@users.noreply.github.com>
2024-03-20 20:19:25 +0530

    Create labone.c

---
+++ labone.c
@@ -0,0 +1,16 @@
+mkdir 3VC20CS065
+ls
+cd 3VC20CS065
+
+Git init
+Git config-list
+git config –global user.name “lingarajpower”
+git config – global user.email “ lingarajpower@gmail.com”
+Git clone https://github.com/lingarajpower/3VC15CS065.git
+cd 3VC15CS065
+git status
+$ echo "hello git and git Hub" >> lab1.txt
+$ git add lab1.txt
+$ git status
+Git commit –m “first msg”
+Git push origin main
